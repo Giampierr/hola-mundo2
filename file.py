e0818d781f3487468362787b8ef0da32d9ff6ba4
Giampierr <132006463+Giampierr@users.noreply.github.com>
2026-03-24 14:59:47 -0500

    Version hecha de git pull y push

--- file.py
+++ file.py
@@ -1,9 +1,5 @@
 print("Hola Peru") 
 
-<<<<<<< HEAD
-print("Este es un ejemplo para la rama/ejemplo") 
-=======
 print("Este es un ejemplo para la rama/ejemplo")
 
 print("Este es un cambio en gitHub")
->>>>>>> f55cd684c484999d2997f38a6ad489166b5bd8f6
